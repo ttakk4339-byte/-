cdf400a9d22d6530b144a804465764acbd48e349
ttakk4339-byte <tt.akk4339@gmail.com>
2026-02-28 12:31:44 +0300

    Create config.py

---
+++ app/core/config.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+DATABASE_URL = os.getenv("DATABASE_URL")
+ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD")
