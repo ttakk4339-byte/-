ca87d9228275c130ed276f2abd68fe65b7c34456
ttakk4339-byte <tt.akk4339@gmail.com>
2026-02-28 12:39:07 +0300

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -3,6 +3,6 @@ import os
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-DATABASE_URL = os.getenv("DATABASE_URL")
-ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD")
+BOT_TOKEN = os.getenv("8594625847:AAF1Q-zrC-PUrV0ox2vtCl5uUkNVR1111Q4")
+DATABASE_URL = os.getenv("DATABASE_URL=sqlite+aiosqlite:///./escrow.db")
+ADMIN_PASSWORD = os.getenv("1133")
